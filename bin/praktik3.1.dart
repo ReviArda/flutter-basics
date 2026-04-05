3ec484605cddd4c0383cb1831e8d8eef8ec3c55a
Revi Arda Saputra <reviarda65@gmail.com>
2026-04-05 16:54:42 +0700

    update komen dan pemahaman

--- bin/praktik3.1.dart
+++ bin/praktik3.1.dart
@@ -1,21 +1,38 @@
+/*
+  Fungsi/Function = sekumpulan kode yang bisa dipanggil berulang kali
+  terdiri dari:
+  1. nama fungsi
+  2. parameter (nilai yang diisi dari luar/si pemanggil)
+  3. memiliki tipe kembalian
+*/
+
+// Di dalam class terdiri dari
 class motor {
+    // 1. Property = variable di dalam class
     String merek;
     String warna;
     int kecepatan;
 
+    // 3. Construktor = Methode yang Namanya sama dengan nama class, digunakan untuk inisiasi
     motor(this.merek, this.warna, this.kecepatan);
 
+    // 2. Methode = fungsi di dalam class
     void gas() {
         kecepatan += 10;
         print("Motor melaju dengan kecepatan $kecepatan km/jam");
     }
 
+    // 2. Methode = fungsi di dalam class
     void rem() {
         kecepatan -= 10;
         print("Motor melambat dengan kecepatan $kecepatan km/jam");
     }
 }
 
+// Contoh Fungsi:
+// 1. Nama fungsi: main
+// 2. Parameter: () -> kosong
+// 3. Tipe kembalian: void -> tidak mengembalikan nilai
 void main() {
     motor motor1 = motor("Honda", "Merah", 0);
     motor1.gas();
